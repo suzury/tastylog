e8be9f01ff64a5a5c412dac2a225a39edb690b47
suzury <suzu.maryy@gmail.com>
2023-11-29 11:44:17 +0900

    トップページアイコン表示されないのとフッターが上に来る問題について

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const PORT = process.env.PORT;
-console.log(`Application Listening at ${PORT}`);
+const path = require("path");
+const express = require("express");
+const favicon = require("serve-favicon");
+const app = express();
+
+// Express settings
+app.set("view engine", "ejs");
+
+//Static resource rooting
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Dynamic resource rooting
+app.use("/", require("./routes/index.js"));
+
+//Execute web application
+app.listen(PORT, () => {
+  console.log(`Application Listening at ${PORT}`);
+});
