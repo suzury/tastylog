a58124a6beaa2a228fbc4b4485474aa07c3098b8
suzury <suzu.maryy@gmail.com>
2023-12-06 15:42:35 +0900

    変数name が定義されていないと出ます。res.renderでは動画の通りに記述していますが動画のres.renderでname はどこで定義しているのか。どこからname  が来ているのかわかりませんでした。

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
@@ -11,6 +11,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
+// Expose global method to view engine.
+app.use((req, res, next) => {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
 //Static resource rooting
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -19,41 +26,8 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(accesslogger());
 
 // Dynamic resource rooting
+app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
-app.use("/test", async (req, res, next) => {
-  const { promisify } = require("util");
-  const path = require("path");
-  const { sql } = require("@garafu/mysql-fileloader")({
-    root: path.join(__dirname, "./lib/database/sql"),
-  });
-  const config = require("./config/mysql.config.js");
-  const mysql = require("mysql");
-  const con = mysql.createConnection({
-    host: config.HOST,
-    port: config.PORT,
-    user: config.USERNAME,
-    password: config.PASSWORD,
-    database: config.DATABASE,
-  });
-  const client = {
-    connect: promisify(con.connect).bind(con),
-    query: promisify(con.query).bind(con),
-    end: promisify(con.end).bind(con),
-  };
-  var data;
-
-  try {
-    await client.connect();
-    data = await client.query(await sql("SELECT_SHOP_BASIC_BY_ID"));
-    console.log(data);
-  } catch (err) {
-    next(err);
-  } finally {
-    await client.end();
-  }
-
-  res.end("OK");
-});
 
 //Set application log.
 app.use(applicationlogger());
---
+++ routes/shops.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { MySQLClient, sql } = require("../lib/database/client.js");
+
+router.get("/:id", async (req, res, next) => {
+  var id = req.params.id;
+
+  Promise.all([
+    MySQLClient.executeQuery(await sql("SELECT_SHOP_DETAIL_BY_ID"), [id]),
+  ])
+    .then((results) => {
+      var data = results[0][0];
+      res.render("./shops/index.ejs", data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
